d40975d8d38973f070394f1cf3870c715519cb22
MaheshPasunoori <pasunoorimahesh.mudiam@gmail.com>
2022-10-29 15:32:17 +0530

    My First Assignment

--- CodeSnippet.java
+++ CodeSnippet.java
@@ -3,7 +3,7 @@ public class CodeSnippet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    int n=7;		
+    int n=13;		
 	for(int i=0;i < n;i++) {
         for(int j=0;j < n;j++) {
         	if(i==0 || i==(n-1) || j==(n-1)/2) {
